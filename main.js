d2843d189f298a1c7a677045f445e8827930d278
咚咚锵 <904885508@qq.com>
2018-07-20 16:47:31 +0800

    首次提交

---
+++ main.js
@@ -0,0 +1,35 @@
+var allButtons = $('div > button');
+
+for (let i = 0; i < allButtons.length; i++) {
+  $(allButtons[i]).on('click', function (x) {
+    colorChange($(allButtons[i]));
+    var index = ($(x.currentTarget).index());
+    var distance = index * -350;
+    // console.log(distance);
+    $('#images').css({
+      transform: 'translateX(' + distance + 'px)'
+    })
+  })
+}
+
+let counter = 1;
+var timeId = setTimer;
+console.log(timeId)
+// console.log($('.window'))
+$('.window').on('mouseenter', function () {
+  clearInterval(timeId);
+})
+$('.window').on('mouseleave', function () {
+  timeId = setTimer();
+})
+
+function setTimer() {
+  return setInterval(() => {
+    // console.log(counter)
+    colorChange(allButtons.eq(counter % 3).trigger('click'));
+    counter += 1;
+  }, 1000)
+}
+function colorChange($button) {
+  $button.addClass('cyan').siblings('.cyan').removeClass('cyan')
+}
